d88a3e1ffc66aacfc4d5a6624a2c317be990b76c
Kriss Gardner <krissrobert12@gmail.com>
2021-01-09 10:48:27 +0200

    Code cleanup

--- src/views/Game/index.tsx
+++ src/views/Game/index.tsx
@@ -15,25 +15,6 @@ const Game: React.FC = () => {
     <div className="game defaultBg">
       <TabsMenu className="game__tabs" />
       <div className="game__main"></div>
-
-      {/* <div className="sideBar">
-        <div className="sideBar__item">
-          <img src={Chat} alt="Chat icon" />
-        </div>
-        <div className="sideBar__item">
-          <img src={Settings} alt="Settings icon" />
-        </div>
-      </div> */}
-
-      {/* <div className="actionBar">
-        <div className="actionBar__item">i</div>
-        <div className="actionBar__item">i</div>
-        <div className="actionBar__item">i</div>
-        <div className="actionBar__item">i</div>
-        <div className="actionBar__item">i</div>
-      </div>
-
-      <Players /> */}
     </div>
   );
 };
